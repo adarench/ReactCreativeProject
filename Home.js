85eb0446e38cbbceeca317fa8456f873127ab48a
adarench <103231117+adarench@users.noreply.github.com>
2022-10-29 22:59:42 -0600

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,14 @@
+import "./Home.css";
+const Home = () => {
+    return (
+        <>
+            <h1>Home</h1>
+            <div><p>hello</p></div>
+        </>
+    );
+        
+    
+    
+  };
+  
+  export default Home;
